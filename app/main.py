ae14da42d8fdf45110e4385da449f2a9fcdc5412
Quentin Bahers <quentin.bahers@gmail.com>
2025-05-04 15:48:40 +0200

    The echo builtin

--- app/main.py
+++ app/main.py
@@ -7,9 +7,14 @@ def main():
 
         # Wait for user input
         command = input()
+        command_split = command.split(" ", 1)
+        command_name, command_argument = (command_split[0], command_split[1]) if len(command_split) > 1 else (command, None)
         if command == "exit 0":
             break
-        print(f"{command}: command not found")
+        elif command_name == "echo":
+            print(command_argument)
+        else:
+            print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
