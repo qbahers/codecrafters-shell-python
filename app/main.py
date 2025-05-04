a6524a3a7b95c17a7dae15ca49b94810fcf8bf0a
Quentin Bahers <quentin.bahers@gmail.com>
2025-05-04 15:39:48 +0200

    REPL

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
+    while True:
+        sys.stdout.write("$ ")
 
-    # Wait for user input
-    command = input()
-    print(f"{command}: command not found")
+        # Wait for user input
+        command = input()
+        print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
